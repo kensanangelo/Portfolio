03da130f574c0e8f592714cf4c06511edbb65d8c
Ken San Angelo <ken@kensanangelo.com>
2019-11-12 13:20:15 -0500

    Refactored js
    Added comments throughout
    Removed scrollTo function and put it's code in nav scroll (didn't make sense to have extra function doing 1 line of code

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,42 +1,52 @@
 $(document).ready(function(){
+  //Hides work slides on load, so they are still accessable without JS
   $(".project-slide").addClass("hidden");
+
   navScroll();
   workSlide();
 });
 
 
-
+//Smooth scrolls to selected section from top nav
 function navScroll(){
   $("nav a").click(function(event) {
-
     if (this.hash !== "") {
       event.preventDefault();
-
       var hash = this.hash;
 
-      scrollTo(hash);
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800);
     }
   });
 }
 
 
+//Scrolls between work sections by showing and hiding sections 
+//while sliding left to right
 function workSlide(){
+  //For main thumb-slide
   $(".thumb").click(function(){
+    //Grabs which slide they are choosing
     var which_slide = $(this).data("order");
 
+    //Slides big slides div to the right
     $(".slides").addClass('project-open');
+
     $("#slide-"+which_slide).removeClass('hidden');
 
+    //Delays hiding slide, so it's less of a noticible pop
     setTimeout(function () {
       $(".thumb-slide").addClass('hidden');
     }, 800);
 
-    scrollTo("#work");
-    $("#work").focus();
-
+    scrollTo("#work"); //Scrolls to top of section (less awkward to start reading from)
+    $("#work").focus(); //For accessability
   });
 
+  //For project slides
   $(".return-arrow").click(function(){
+    //Grabs which slide they are on
     var which_slide = $(this).data("slide");
     
     $(".slides").removeClass('project-open');
@@ -49,11 +59,4 @@ function workSlide(){
     scrollTo("#work");
     $("#work").focus();
   });
-}
-
-
-function scrollTo(location) {
-  $('html, body').animate({
-      scrollTop: $(location).offset().top
-  }, 800);
 }
